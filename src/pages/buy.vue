cc9c1e0f1eef8fa3da806ca133d48452732f4e47
vivian <tianwenwen555@sina.com>
2017-06-08 17:09:18 +0800

    基础

---
+++ src/pages/buy.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      {{$route.params.id}}
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+
+            }
+        }
+    }
+
+</script>
+<style lang="less">
+
+</style>
