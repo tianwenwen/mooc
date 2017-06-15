c410efdcd6f962b5279d7714f305deccb9caebbb
vivian <tianwenwen555@sina.com>
2017-06-15 17:16:03 +0800

    lodash

--- src/pages/detail/count.vue
+++ src/pages/detail/count.vue
@@ -1,17 +1,52 @@
 <template>
-    <div>
-        count
-    </div>
+  <div>
+    <a @click="getPrice">count</a>
+    {{price.amount}}
+  </div>
 </template>
 
 <script>
-    export default {
-        data: function () {
-            return {
-
-            }
+  import _ from 'lodash'
+  export default {
+    data: function () {
+      return {
+        buyNumber: '',
+        buyType: '',
+        versions: [
+          {
+            value:'2'
+          },
+          {
+            value:'1.2'
+          }
+        ],
+        period: '',
+        price:''
+      }
+    },
+    mounted:{
+      //渲染完
+    },
+    methods: {
+      getPrice:function(){
+        var buyVersion = _.map(this.versions,function(item){
+          return item.value;
+        })
+        var params ={
+          buyNumber:this.buyNumber,
+          buyType:this.buyType,
+          version:buyVersion.join(','),
+          period:this.period
         }
+
+        this.$http.post('/api/getPrice',params).then(function(data){
+            if(data.status == '200'){
+              this.price = data.data
+            }
+        })
+      }
     }
+  }
 
 </script>
 <style lang="less">
